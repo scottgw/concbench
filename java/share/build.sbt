9592840cc637be3d55debe1b2cb46ff5c19b4570
Scott West <scott.gregory.west@gmail.com>
2013-02-10 22:20:59 +0100

    Sbt-ing Java Share benchmark.

---
+++ java/share/build.sbt
@@ -0,0 +1,6 @@
+name := "Share"
+
+version := "1.0"
+
+autoScalaLibrary := false
+
---
+++ java/share/src/main/java/Share.java
@@ -0,0 +1,46 @@
+package ch.ethz.se.concbench.share;
+
+import java.util.Vector;
+
+public class Share {
+
+    static class Worker implements Runnable {
+        int [] shared;
+		
+        Worker (int[] s) {
+            shared = s;
+        }
+		
+        public void run() {
+            for (int i = 0; i < arraySize; i++) {
+                shared[i]++;
+            }
+        }
+    }
+	
+    static int arraySize = 5000;
+	
+    public static void main(String[] args) {
+        arraySize = Integer.parseInt(args[0]);
+        int n = Integer.parseInt(args[1]);
+
+        Vector<Thread> threads = new Vector<Thread>();
+        int[] shared = new int[arraySize];
+
+        for (int i = 0; i < n; i++) {
+            Thread t = new Thread(new Worker(shared));
+            threads.add(t);
+            t.start();
+        }
+
+        for (Thread t : threads) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
