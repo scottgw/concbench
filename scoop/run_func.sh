628f27381655f7c9a0bd8e8abc6c33634aec3202
Scott West <scott.gregory.west@gmail.com>
2013-02-12 10:06:05 +0100

    Adding run script to condition.

---
+++ scoop/run_func.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+TIMEOUT=180s
+
+function run_loop {
+    EXEC=EIFGENs/$1/F_code/$@
+    TIMEOUT_EXEC="timeout -s 9 $TIMEOUT time -f "%e" $EXEC 2>&1 >/dev/null"
+
+    # redirect stderr to stdout and stdout to /dev/null
+    out=$($TIMEOUT_EXEC)
+
+    while [ $? -ne 0 ]; do
+        out=$($TIMEOUT_EXEC)
+    done
+
+    echo $out
+}
