11c2d18564f76f9d1fa13146e0ae45d3baa56ac8
Scott West <scott.gregory.west@gmail.com>
2013-05-30 07:26:01 +0200

    Updating qs mutex for new API.

--- qs/mutex/main.c
+++ qs/mutex/main.c
@@ -18,6 +18,8 @@
 
 #define MAX_TASKS 20000
 
+extern int num_dequeue;
+
 int num_each;
 int num_iters;
 
@@ -36,10 +38,11 @@ worker(processor_t proc, processor_t shared)
 {
   void ***args;
   clos_type_t *arg_types;
-  priv_queue_t q = priv_queue_new(shared);
-
+  priv_queue_t q = NULL;
   for (int i = 0; i < num_iters; i++)
     { 
+      q = proc_get_queue(proc, shared);
+
       closure_t clos =
         closure_new(action,
                     closure_void_type(),
@@ -51,19 +54,18 @@ worker(processor_t proc, processor_t shared)
       
       *args[0] = shared;
 
-      priv_queue_lock(q, shared, proc);
+      priv_queue_lock(q, proc);
       priv_queue_routine(q, clos, proc);
       priv_queue_unlock(q, proc);
     }
 
-  priv_queue_shutdown(q, shared, proc);
-  printf("worker pre shutdown\n");
+  /* printf("worker pre shutdown\n"); */
   proc_shutdown(proc, proc);
 
-  printf("worker shutdown\n");
+  /* printf("worker shutdown\n"); */
   if( __sync_add_and_fetch(&num_finished, 1) == num_each)
     {
-      printf("shared shutdown %p\n", shared);
+      /* printf("shared shutdown %p\n", shared); */
       proc_shutdown(shared, proc);
     }
 }
@@ -71,14 +73,14 @@ worker(processor_t proc, processor_t shared)
 void
 proc_main(processor_t proc)
 {
-  printf("mutex main\n");
-  processor_t shared = make_processor(proc->task->sync_data);
-  
+  printf("mutex main %p\n", proc);
+  processor_t shared = proc_new(proc->task->sync_data);
+  printf("mutex shared %p\n", shared);
   for (int i = 0; i < num_each; i++)
     {
       printf("creating worker %d\n", i);
-      processor_t worker_proc = make_processor(proc->task->sync_data);
-      priv_queue_t q = priv_queue_new(worker_proc);
+      processor_t worker_proc = proc_new(proc->task->sync_data);
+      priv_queue_t q = proc_get_queue(proc, worker_proc);
       
       void ***args;
       clos_type_t *arg_types;
@@ -96,12 +98,11 @@ proc_main(processor_t proc)
       *args[0] = worker_proc;
       *args[1] = shared;
 
-      priv_queue_lock(q, worker_proc, proc);
+      priv_queue_lock(q, proc);
       priv_queue_routine(q, clos, proc);
       priv_queue_unlock(q, proc);
-
-      priv_queue_shutdown(q, worker_proc, proc);
     }
+  proc_deref_priv_queues(proc);
 }
 
 int
@@ -110,7 +111,7 @@ main(int argc, char **argv)
   num_iters = atoi(argv[1]);
   num_each  = atoi(argv[2]);
   sync_data_t sync_data = sync_data_new(MAX_TASKS);
-  processor_t proc = make_root_processor(sync_data, proc_main);
+  processor_t proc = proc_new_root(sync_data, proc_main);
 
   create_executors(sync_data, 4);
 
